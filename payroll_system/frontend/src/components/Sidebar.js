99edab0261bd433902551522c153d256b9037467
Paolo Gonzales <pickelse25@gmail.com>
2021-05-03 17:31:35 +0800

    add basic responsiveness to nav

--- payroll_system/frontend/src/components/Sidebar.js
+++ payroll_system/frontend/src/components/Sidebar.js
@@ -10,52 +10,79 @@ export default function Sidebar() {
     const showSidebar = () => setSidebar(!sidebar);
     const [state, setState] = useState([]);
 
-    //send this state to the data class of employee to render the search
+    //? for the responsivness
+    const [act, setAct] = useState(false);
+    const [navs, setNavs] = useState(true)
+
+    //!send this state to the data class of employee to render the search
     const [trigSearch, setTrigSearch] = useState(false)
 
-    //lagay mo to
+    // !triggers render
     const [rend, setRend] = useState(false);
     
-    //gets the input in search bar
-    function getSearch(val){
+    //?gets the input in search bar
+    function get_search(val){
         setState(val.target.value);
     }
     
-    function onPress(){
+    function on_press(){
          console.log(state)
          setRend(true)
          setTrigSearch(true)
     }
+
+    // //?check the size of the screen
+    function show_menu_bars(){
+        if(window.innerWidth <= 1000){
+            setAct(true)
+            setNavs(false)
+        }
+        else{
+            setAct(false)
+            setNavs(true)
+        }
+    }
+
+    window.addEventListener('resize', show_menu_bars)
+    window.onload = () => show_menu_bars();
+
+
+
     return (
         <>
             <div className="navbar">
                 <div className="cont">
-                    <h1 className="clk" onClick={() => {window.location.pathname = "/dashboard"}}>Vertex One</h1>
-                    <div className="logs">
-                        {SideBarData.map((val, key) =>{
-                            return (
-                                <div className="ic" key={key} onClick={() => {window.location.pathname = val.link}}>
-                                    <div className="nav-icon">{val.icon}</div>
-                                    <div className="nav-title">{val.title}</div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    
+                    <h1 className="clk" id={act ? 'clk-act' : ''} onClick={() => {window.location.pathname = "/dashboard"}}>Vertex One</h1>
+                    {navs ?
+                        <div className="logs">
+                            {SideBarData.map((val, key) =>{
+                                return (
+                                    <div className="ic" key={key} onClick={() => {window.location.pathname = val.link}}>
+                                        <div className="nav-icon">{val.icon}</div>
+                                        <div className="nav-title">{val.title}</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    : ''}
                 </div>
                 
                 {/* search bar */}
                 <div className="bx">
                     <div className="search-box">
-                        <input type='text' name='search' onChange={getSearch} placeholder='Search Employee'/>
+                        <input type='text' name='search' onChange={get_search  } placeholder='Search Employee'/>
                     </div>
-                    <div className="xx" onClick={onPress}>
+                    <div className="xx" onClick={on_press} id={act ? '' : 'xx-act'}>
                         <IoSearch/>
                     </div>
                     <EmployeeSearch state={state} rend={rend} setRend={setRend} trigSearch={trigSearch} setTrigSearch={setTrigSearch}/>
-                    {/* <div className="menu-bars" onClick={showSidebar}>
-                        {sidebar ? <FaTimes/> : <FaBars/>}
-                    </div> */}
+                        
+                    {/* menu bars */}
+                    {act ? 
+                        <div className="menu-bars" onClick={showSidebar}>
+                            {sidebar ? <FaTimes/> : <FaBars/>}
+                        </div>  
+                    : ''}
                 </div>
                 
             </div>
