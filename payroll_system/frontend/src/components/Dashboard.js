de70a946120290d892d8ecb493e4374d8b43ae46
Paolo Gonzales <pickelse25@gmail.com>
2021-05-04 13:04:02 +0800

    finished dashboard frontend

--- payroll_system/frontend/src/components/Dashboard.js
+++ payroll_system/frontend/src/components/Dashboard.js
@@ -2,11 +2,27 @@ import React from 'react'
 import Sidebar from './Sidebar'
 import DoughnutCharts from './DoughnutCharts'
 import LineChart from './LineChart'
+import {CardsData} from './CardsData'
 
 export default function Dashboard() {
     return (
         <div className="dashboard">
-            <div className="card">
+            <div className="dash-card">
+
+                {CardsData.map((val, key) => {
+                    return(
+                        <div className="card" key={key}>
+                            <div className="line-label">
+                                <p>{val.label}</p>
+                                <h1>{val.val}</h1>
+                            </div>
+                            <div className="dash-ico" id={val.id}>
+                                {val.img}
+                            </div>
+                        </div>
+                    )
+                })}
+
 
             </div>
             <div className="charts">
@@ -15,6 +31,22 @@ export default function Dashboard() {
                         <p>Monthly Expenses</p>
                     </div>
                     <div className="line-val">
+                        <div className="line-label">
+                            <p>PagIBIG</p>
+                            <h1>$ 40k</h1>
+                        </div>
+                        <div className="line-label">
+                            <p>SSS</p>
+                            <h1>$ 29k</h1>
+                        </div>
+                        <div className="line-label">
+                            <p>Gross Salary</p>
+                            <h1>$ 70k</h1>
+                        </div>
+                        <div className="line-label">
+                            <p>PHILHEALTH</p>
+                            <h1>$ 13k</h1>
+                        </div>
 
                     </div>
                     <div className="line-chart">
--- payroll_system/frontend/src/components/LineChart.js
+++ payroll_system/frontend/src/components/LineChart.js
@@ -21,56 +21,52 @@ function LineChart() {
                     datasets: [
                         {
                             label: 'PagIBIG',
-                            data: [65, 59, 80, 81, 56, 55, 40],
-                            fill: true,
-                            borderColor: 'rgb(75, 192, 192)',
+                            data: [43, 29, 25, 35, 36, 55, 40],
+                            fill: {
+                                target: 'origin',
+                                above: 'rgba(73, 119, 173, 0.4)', 
+                              },
+                            borderColor: 'rgb(73, 119, 173)',
                             tension: 0.5
                         },
                         {
                             label: 'SSS',
                             data: [65, 30, 40, 11, 56, 55, 29],
-                            fill: true,
-                            borderColor: 'rgb(73, 119, 173)',
+                            fill:{
+                                target: 'origin',
+                                above: 'rgba(69, 123, 165, 0.5)'
+                            },
+                            borderColor: 'rgb(69, 123, 165)',
                             tension: 0.5
                         },
                         {
                             label: 'Gross Salary',
-                            data: [70, 32, 25, 31, 70, 90, 29],
-                            fill: true,
-                            borderColor: 'rgb(73, 119, 173)',
+                            data: [70, 32, 25, 31, 70, 90, 70],
+                            fill:{
+                                target: 'origin',
+                                above: 'rgba(97, 158, 190, 0.8)'
+                            },
+                            borderColor: 'rgb(97, 158, 190)',
                             tension: 0.5
                         },
                         {
                             label: 'PHILHEALTH',
                             data: [40, 21, 46, 21, 59, 89, 13],
                             fill: true,
-                            borderColor: 'rgb(73, 119, 173)',
+                            borderColor: 'rgb(141, 189, 212)',
                             tension: 0.5
                         },
                     ]
 
                 }}
-                width={"30%"}
                 options={
                     { 
-                         
-                        options: {
-                            responsive: true,
-                            scales: {
-                              xAxes: [{
-                                gridLines: {
-                                  display: true
-                                }
-                              }],
-                              yAxes: [{
-                                gridLines: {
-                                  display: false
-                                }
-                              }]
-                              }
-                        },
-                  
                         maintainAspectRatio: false,
+                        scales: {
+                            y: {
+                                display : false
+                            }
+                          }
                     }
                 }
             />
